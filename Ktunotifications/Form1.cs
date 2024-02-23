ac88a4c5ab0ff4972086afaa6f321cad29feec7a
JoelVargis <joelvargisthomas2995@gmail.com>
2024-02-23 21:16:31 +0530

    version 0.1

---
+++ Ktunotifications/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ktunotifications
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closebutton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+            panel3.BackColor = Color.White;  
+            textBox2.BackColor = SystemColors.Menu;
+            panel4.BackColor = SystemColors.Menu;
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.BackColor = Color.White;
+            panel4.BackColor = Color.White;
+            textBox1.BackColor = SystemColors.Menu;
+            panel3.BackColor = SystemColors.Menu;
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+             pictureBox5.Visible= false;
+            pictureBox4.Visible= true;
+            textBox2.UseSystemPasswordChar=false;   
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Visible = true;
+            pictureBox4.Visible = false;
+            textBox2.UseSystemPasswordChar = true;
+        }
+    }
+}
